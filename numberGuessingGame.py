0541e5f9066488714f823fa4c984df113689e291
Vyom-2412 <73429279+Vyom-2412@users.noreply.github.com>
2022-04-02 12:34:48 +0530

    Add files via upload

---
+++ numberGuessingGame.py
@@ -0,0 +1,23 @@
+import random
+print("NUmber Guessing Game")
+number=random.randint(1,10)
+chances = 0
+print("Guess a number between 1 and 10:")
+
+while(chances < 5):
+    guess = int(input("Enter your guess :- "))
+
+    if(guess == number):
+        print("Congratulations You Won!! :D")
+        break
+
+    elif(guess < number):
+        print("Your guess was too low. (Hint : Guess a number higher than", guess)
+    
+    else:
+        print("Your guess was too high. (Hint : Guess a number lower than", guess)
+
+    chances +=1
+
+if not(chances < 5):
+    print("YOU LOSE!! The number was", number)
